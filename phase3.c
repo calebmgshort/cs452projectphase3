5f0fd867dda2afe709d6121a55bff700edd7a696
Taylor Heimbichner <theimbichner@email.arizona.edu>
2017-10-04 11:23:36 -0700

    Added stubs

---
+++ phase3.c
@@ -0,0 +1,54 @@
+#include <usloss.h>
+
+int debugflag3 = 0;
+
+int start2(char *arg)
+{
+    if (DEBUG3 && debugflag3)
+    {
+        USLOSS_Console("start3(): Called.\n");
+    }
+    
+}
+
+void spawn(systemArgs *arg)
+{
+}
+
+void wait(systemArgs *args)
+{
+}
+
+void terminate(systemArgs *args)
+{
+}
+
+void semCreate(systemArgs *args)
+{
+}
+
+void semP(systemArgs *args)
+{
+}
+
+void semV(systemArgs *args)
+{
+}
+
+void semFree(systemArgs *args)
+{
+}
+
+void getTimeOfDay(systemArgs *args)
+{
+}
+
+void CPUTime(systemArgs *args)
+{
+}
+
+void getPID(systemArgs *args)
+{
+}
+
+
