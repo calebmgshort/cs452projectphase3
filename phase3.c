8c314d13defce644615af37b3c806c21417c32b2
Taylor Heimbichner <theimbichner@email.arizona.edu>
2017-10-04 11:49:43 -0700

    Initialize syscall vec, start spawning start3

--- phase3.c
+++ phase3.c
@@ -1,4 +1,8 @@
 #include <usloss.h>
+#include <usyscall.h>
+
+#include "phase1.h"
+#include "phase2.h"
 
 int debugflag3 = 0;
 
@@ -8,7 +12,36 @@ int start2(char *arg)
     {
         USLOSS_Console("start3(): Called.\n");
     }
-    
+
+    // initialize system call vector
+    for (int i = 0; i < MAXSYSCALL; i++)
+    {
+        systemCallVec[i] = nullsys3;
+    }
+    systemCallVec[SYS_SPAWN] = spawn;
+    systemCallVec[SYS_WAIT] = wait;
+    systemCallVec[SYS_TERMINATE] = terminate;
+    systemCallVec[SYS_SEMCREATE] = semCreate;
+    systemCallVec[SYS_SEMP] = semP;
+    systemCallVec[SYS_SEMV] = semV;
+    systemCallVec[SYS_SEMFREE] = semFree;
+    systemCallVec[SYS_GETTIMEOFDAY] = getTimeOfDay;
+    systemCallVec[SYS_CPUTIME] = cpuTime;
+    systemCallVec[SYS_GETPID] = getPID;
+
+    // spawn start 3
+    systemArgs args;
+    args.number = SYS_SPAWN;
+    args.arg1 = (void *) start3;
+    args.arg2 = NULL;
+    args.arg3 = (void *) (4 * USLOSS_MIN_STACK);
+    args.arg4 = (void *) 3;
+    args.arg5 = (void *) "start3";
+    USLOSS_Syscall((void *) &args);
+}
+
+void nullsys3(systemArgs *arg)
+{
 }
 
 void spawn(systemArgs *arg)
@@ -43,7 +76,7 @@ void getTimeOfDay(systemArgs *args)
 {
 }
 
-void CPUTime(systemArgs *args)
+void cpuTime(systemArgs *args)
 {
 }
 
