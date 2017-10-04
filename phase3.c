b7025f4d40260d378d0ff9cb0047a06d3c340ca3
Caleb Short <Caleb@Calebs-MBP-2.PK5001Z>
2017-10-04 12:17:06 -0700

    Wrote semCreate

--- phase3.c
+++ phase3.c
@@ -1,5 +1,25 @@
 #include <usloss.h>
 
+#define NO_SLOTS_AVAILABLE -1
+
+typedef struct USLOSS_Sysargs
+{
+        int  number;
+        void *arg1;
+        void *arg2;
+        void *arg3;
+        void *arg4;
+        void *arg5;
+} systemArgs;
+
+typedef struct
+{
+    int count;
+} Semaphore;
+
+// TODO: There is some limit on semaphores. Fix this
+Semaphore[] semaphores = Semaphore[50];
+
 int debugflag3 = 0;
 
 int start2(char *arg)
@@ -8,7 +28,7 @@ int start2(char *arg)
     {
         USLOSS_Console("start3(): Called.\n");
     }
-    
+
 }
 
 void spawn(systemArgs *arg)
@@ -25,6 +45,29 @@ void terminate(systemArgs *args)
 
 void semCreate(systemArgs *args)
 {
+    int initSemValue = *((int*) args->arg1)); // TODO: use val in pointer or pointer?
+    int* args1Val = &(-1);
+    if(initSemValue < 0)
+    {
+        args->arg1 = (void*) args1Val;
+        args->arg4 = (void*) args1Val;
+        return;
+    }
+    int handle = getAvailableHandle();  // TODO: implement
+    if(hande == NO_SLOTS_AVAILABLE)
+    {
+        args->arg1 = (void*) args1Val;
+        args->arg4 = (void*) args1Val;
+        return;
+    }
+    *args1Val = handle;
+
+    Semaphore *sem;
+    sem->count = initSemValue
+    semaphores[handle] = sem;
+    args->arg1 = (void*) args1Val;
+    args->arg4 = (void*) &(0);
+    return;
 }
 
 void semP(systemArgs *args)
@@ -50,5 +93,3 @@ void CPUTime(systemArgs *args)
 void getPID(systemArgs *args)
 {
 }
-
-
