9f689f8817c880664489ea1f38ee212b6945b679
Taylor Heimbichner <theimbichner@email.arizona.edu>
2017-10-06 10:55:21 -0700

    Reverted phase3.h to original value and made sems.h

--- phase3.h
+++ phase3.h
@@ -7,21 +7,4 @@
 
 #define MAXSEMS         200
 
-#define NO_SLOTS_AVAILABLE -1
-
-typedef struct USLOSS_Sysargs
-{
-        int  number;
-        void *arg1;
-        void *arg2;
-        void *arg3;
-        void *arg4;
-        void *arg5;
-} systemArgs;
-
-typedef struct
-{
-    int count;
-} Semaphore;
-
 #endif /* _PHASE3_H */
