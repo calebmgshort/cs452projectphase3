f631a547f9f095ddfc23f73899e6d0788d7b99a3
Caleb Short <Caleb@Calebs-MBP-2.PK5001Z>
2017-10-05 20:54:39 -0700

    Just did some stuff. It all needs to be changed

--- libuser.c
+++ libuser.c
@@ -35,11 +35,11 @@
  *  Return Value: 0 means success, -1 means error occurs
  *
  */
-int Spawn(char *name, int (*func)(char *), char *arg, int stack_size, 
-    int priority, int *pid)   
+int Spawn(char *name, int (*func)(char *), char *arg, int stack_size,
+    int priority, int *pid)
 {
     USLOSS_Sysargs sysArg;
-    
+
     CHECKMODE;
     sysArg.number = SYS_SPAWN;
     sysArg.arg1 = (void *) func;
@@ -71,7 +71,7 @@ int Spawn(char *name, int (*func)(char *), char *arg, int stack_size,
 int Wait(int *pid, int *status)
 {
     USLOSS_Sysargs sysArg;
-    
+
     CHECKMODE;
     sysArg.number = SYS_WAIT;
 
@@ -80,14 +80,14 @@ int Wait(int *pid, int *status)
     *pid = (int) sysArg.arg1;
     *status = (int) sysArg.arg2;
     return (int) sysArg.arg4;
-    
+
 } /* end of Wait */
 
 
 /*
  *  Routine:  Terminate
  *
- *  Description: This is the call entry to terminate 
+ *  Description: This is the call entry to terminate
  *               the invoking process and its children
  *
  *  Arguments:   int status -- the commpletion status of the process
@@ -97,7 +97,15 @@ int Wait(int *pid, int *status)
  */
 void Terminate(int status)
 {
-    
+  USLOSS_Sysargs sysArg;
+
+  CHECKMODE;
+  sysArg.number = SYS_TERMINATE;
+
+  USLOSS_Syscall(&sysArg);
+
+  return (int) sysArg.arg1;
+
 } /* end of Terminate */
 
 
@@ -169,7 +177,7 @@ int SemFree(int semaphore)
  *  Arguments:
  *
  */
-void GetTimeofDay(int *tod)                           
+void GetTimeofDay(int *tod)
 {
 } /* end of GetTimeofDay */
 
@@ -182,7 +190,7 @@ void GetTimeofDay(int *tod)
  *  Arguments:
  *
  */
-void CPUTime(int *cpu)                           
+void CPUTime(int *cpu)
 {
 } /* end of CPUTime */
 
@@ -195,7 +203,7 @@ void CPUTime(int *cpu)
  *  Arguments:
  *
  */
-void GetPID(int *pid)                           
+void GetPID(int *pid)
 {
 } /* end of GetPID */
 
--- phase3.c
+++ phase3.c
@@ -16,6 +16,13 @@ int start2(char *arg)
         USLOSS_Console("start2(): Called.\n");
     }
 
+pid = spawnReal("start3", start3, NULL, USLOSS_MIN_STACK, 3);
+
+/* Call the waitReal version of your wait code here.
+ * You call waitReal (rather than Wait) because start2 is running
+ * in kernel (not user) mode.
+ */
+
     // Check kernel mode
     if (!(USLOSS_PsrGet() & USLOSS_PSR_CURRENT_MODE))
     {
@@ -38,6 +45,35 @@ int start2(char *arg)
     systemCallVec[SYS_CPUTIME] = cpuTime;
     systemCallVec[SYS_GETPID] = getPID;
 
+    /*
+     * Create first user-level process and wait for it to finish.
+     * These are lower-case because they are not system calls;
+     * system calls cannot be invoked from kernel mode.
+     * Assumes kernel-mode versions of the system calls
+     * with lower-case names.  I.e., Spawn is the user-mode function
+     * called by the test cases; spawn is the kernel-mode function that
+     * is called by the syscallHandler; spawnReal is the function that
+     * contains the implementation and is called by spawn.
+     *
+     * Spawn() is in libuser.c.  It invokes USLOSS_Syscall()
+     * The system call handler calls a function named spawn() -- note lower
+     * case -- that extracts the arguments from the sysargs pointer, and
+     * checks them for possible errors.  This function then calls spawnReal().
+     *
+     * Here, we only call spawnReal(), since we are already in kernel mode.
+     *
+     * spawnReal() will create the process by using a call to fork1 to
+     * create a process executing the code in spawnLaunch().  spawnReal()
+     * and spawnLaunch() then coordinate the completion of the phase 3
+     * process table entries needed for the new process.  spawnReal() will
+     * return to the original caller of Spawn, while spawnLaunch() will
+     * begin executing the function passed to Spawn. spawnLaunch() will
+     * need to switch to user-mode before allowing user code to execute.
+     * spawnReal() will return to spawn(), which will put the return
+     * values back into the sysargs pointer, switch to user-mode, and
+     * return to the user code that called Spawn.
+     */
+
     // spawn start 3
     int pid = spawnReal("start3", start3, NULL, 4 * USLOSS_MIN_STACK, 3);
 
@@ -46,22 +82,27 @@ int start2(char *arg)
     pid = waitReal(&status);
 }
 
+// TODO: Implement
 void nullsys3(systemArgs *arg)
 {
 }
 
+// TODO: Implement
 void spawn(systemArgs *arg)
 {
 }
 
+// TODO: Implement
 void wait(systemArgs *args)
 {
 }
 
+// TODO: Implement
 void terminate(systemArgs *args)
 {
 }
 
+// TODO: Implement
 void semCreate(systemArgs *args)
 {
     int initSemValue = *((int*) args->arg1)); // TODO: use val in pointer or pointer?
@@ -89,26 +130,85 @@ void semCreate(systemArgs *args)
     return;
 }
 
+// TODO: Implement
 void semP(systemArgs *args)
 {
 }
 
+// TODO: Implement
 void semV(systemArgs *args)
 {
 }
 
+// TODO: Implement
 void semFree(systemArgs *args)
 {
 }
 
+// TODO: Implement
 void getTimeOfDay(systemArgs *args)
 {
 }
 
+// TODO: Implement
 void cpuTime(systemArgs *args)
 {
 }
 
+// TODO: Implement
 void getPID(systemArgs *args)
+{
+}
+
+/******************** Real Functions ******************/
+
+
+// TODO: Implement
+void spawnReal()
+{
+}
+
+// TODO: Implement
+void waitReal()
+{
+}
+
+// TODO: Implement
+void terminateReal()
+{
+}
+
+// TODO: Implement
+void semCreateReal()
+{
+}
+
+// TODO: Implement
+void semPReal()
+{
+}
+
+// TODO: Implement
+void semVReal()
+{
+}
+
+// TODO: Implement
+void semFreeReal()
+{
+}
+
+// TODO: Implement
+void getTimeOfDayReal()
+{
+}
+
+// TODO: Implement
+void cpuTimeReal()
+{
+}
+
+// TODO: Implement
+void getPIDReal()
 {
 }
